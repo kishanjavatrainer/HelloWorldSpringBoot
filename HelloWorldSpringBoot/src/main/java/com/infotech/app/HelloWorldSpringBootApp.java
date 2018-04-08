44c53afb5bdf6a6c159cc9344c986fd1839ed300
SumukhDeshpande <sumukhmailings@gmail.com>
2018-04-08 10:15:33 +0530

    Update HelloWorldSpringBootApp.java
    Without component scan annotation I do not see "2018-04-08 10:03:18.390  INFO 13668 --- [           main] s.w.s.m.m.a.RequestMappingHandlerMapping : Mapped "{[/]}" onto public java.lang.String org.learn.springboot.controller.HelloController.hello()" this in console.
    
    And that is the reason ResfFul controller does not return anything to the URL requested.
    
    If without the ComponentScan it still works then could you please help to find.

--- HelloWorldSpringBoot/src/main/java/com/infotech/app/HelloWorldSpringBootApp.java
+++ HelloWorldSpringBoot/src/main/java/com/infotech/app/HelloWorldSpringBootApp.java
@@ -2,8 +2,10 @@ package com.infotech.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.infotech.app.controller")
 public class HelloWorldSpringBootApp {
 
 	public static void main(String[] args) {
